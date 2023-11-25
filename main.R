9ab576b74c9d15e1b2af0be205e99d1b8b0078de
alantao5056 <alantaolr@outlook.com>
2023-11-25 18:20:35 -0500

    make plots

--- main.R
+++ main.R
@@ -2,6 +2,11 @@ library(tidyverse)
 library(dplyr)
 
 hdigdp = read_csv("./data/hdigdp.csv")
+
+hdigdp = hdigdp |>
+  mutate(year = Year, HDI = `Human Development Index`, country = Entity, GDPPC = `GDP per capita, PPP (constant 2017 international $)`) |>
+  select(country, year, HDI, GDPPC)
+
 schooling = read_csv("./data/schooling.csv")
 
 schooling = schooling |>
@@ -14,3 +19,53 @@ suicide = read_csv("./data/suicide.csv")
 
 full = full_join(suicide, schooling, by = c("country", "year"))
 
+full = left_join(full, hdigdp, by = c("country", "year"))
+
+# gdp hdi for every country
+full |> 
+  drop_na() |>
+  summarize(.by = country, avg_hdi = mean(`HDI`), avg_gdp = mean(`GDPPC`)) |>
+  ggplot(aes(x = avg_gdp, y = avg_hdi)) +
+  geom_point() +
+  geom_smooth(se = FALSE, method = "lm", formula = (y ~ log(x))) +
+  labs(title = "GDP vs HDI for Every Country",
+       x = "Average GDP",
+       y = "Average HDI")
+
+# GDP in the us
+full |>
+  filter(country == "United States") |>
+  summarize(.by = year, avg_hdi = mean(`GDPPC`)) |>
+  ggplot(aes(x = year, y = avg_hdi)) +
+  geom_line() +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  xlim(1990, 2017) +
+  labs(title = "GDP Per Capita in the US",
+       x = "Year",
+       y = "GDP Per Capita")
+# hdi in us
+full |>
+  filter(country == "United States") |>
+  summarize(.by = year, avg_hdi = mean(`HDI`)) |>
+  ggplot(aes(x = year, y = avg_hdi)) +
+  geom_line() +
+  scale_y_continuous(labels = scales::dollar_format()) +
+  xlim(1990, 2017) +
+  labs(title = "HDI in the US",
+       x = "Year",
+       y = "HDI")
+# suicide
+full |>
+  select(year, `suicides/100k pop`) |>
+  drop_na() |>
+  summarize(.by = year, avg_suicide = mean(`suicides/100k pop`)) |>
+  ggplot(aes(x = year, y = avg_suicide)) +
+  geom_line() +
+  labs(title = "Average Suicide Rates in the World",
+       subtitle = "spiked in 1995",
+       x = "Year",
+       y = "Average Suicide per 100k People")
+
+ temp = full |>
+  filter(country == "United States") |>
+  summarize(.by = year, avg_hdi = mean(`HDI`))
