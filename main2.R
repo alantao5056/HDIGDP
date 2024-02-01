ac08ceb1b717927d62d1e1a9e4cba3a57fdde171
alantao5056 <alantaolr@outlook.com>
2024-02-01 16:42:46 -0500

    plots of project phase

---
+++ main2.R
@@ -0,0 +1,23 @@
+ESchooling = read_csv("./data/expected-years-of-schooling.csv")
+
+ESCountry = ESchooling |>
+  filter(Entity=="United States" | Entity == "China" | Entity == "United Kingdom" | Entity == "Russia" | Entity == "Germany")
+ESCountry |>
+  ggplot(aes(x=Year, y=`Expected years of schooling`)) +
+  geom_line(aes(x=Year, color = Entity))
+
+MSchooling = read_csv("./data/mean-years-of-schooling-long-run.csv")
+MSCountry = MSchooling |>
+  filter(Entity=="United States" | Entity == "China" | Entity == "United Kingdom" | Entity == "Russia" | Entity == "Germany")
+MSCountry |>
+  ggplot(aes(x=Year, y=`Combined - average years of education for 15-64 years male and female youth and adults`)) +
+  geom_line(aes(x=Year, color = Entity)) +
+  labs(y = "Mean years of schooling")
+
+
+GNI = read_csv("./data/gross-national-income-per-capita-undp.csv")
+GNICountry = GNI |>
+  filter(Entity=="United States" | Entity == "China" | Entity == "United Kingdom" | Entity == "Russia" | Entity == "Germany")
+MSCountry |>
+  ggplot(aes(x=Year, y=`Gross national income per capita`)) +
+  geom_line(aes(x=Year, color = Entity))
